0c7b3d930308b382162b5882a71112b6705e9d7d
PapaniChethan <papanichethan09@gmail.com>
2025-01-26 18:26:08 +0530

    Character lowercase or uppercase or digit or special

---
+++ ExcelR-Training-Batch1/Core Java/Demo040.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Demo040 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a single character: ");
+        char input = sc.next().charAt(0);
+
+        if(Character.isUpperCase(input)){
+            System.out.println("Character is in Upper case.");
+        }else if(Character.isLowerCase(input)){
+            System.out.println("Character is in Lower case.");
+        }else if(Character.isDigit(input)){
+            System.out.println("Character is a Digit.");
+        }else{
+            System.out.println("Entered character is a special character.");
+        }
+        }
+                
+    }
+    
