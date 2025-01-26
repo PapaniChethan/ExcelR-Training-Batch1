2ef1af586672d1688de2295606239f9f89296eec
PapaniChethan <papanichethan09@gmail.com>
2025-01-26 19:37:10 +0530

    Checking username and password 2

---
+++ ExcelR-Training-Batch1/Core Java/Demo007.java
@@ -0,0 +1,13 @@
+class demo007 {
+    @SuppressWarnings("StringEquality")
+    public static void main(String[] args) {
+
+        String Username = "Mani";
+        String Password = "Mani417";
+
+        while((Username == "Mani") && (Password == "Mani417")) {
+            System.out.println("Welcome to Mallareddy university");
+            break;
+        }
+    }
+ }
